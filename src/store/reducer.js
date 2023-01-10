47a83641535e759ca04cabd35f07a67cdbb2d42e
17BEC0881 <karunanithiswetha1999@gmail.com>
2023-01-10 19:07:04 +0530

    reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,3 @@
-// import { CREATE_FARMER, DISTRICT, PANCHAYAT, STATE, UNION, VILLAGE } from './action';
 import { createSlice } from "@reduxjs/toolkit";
 
 export const FarmerDetails = createSlice({
@@ -6,6 +5,7 @@ export const FarmerDetails = createSlice({
   initialState:{
     farmer: [],
     farmer_id: [],
+    editData: {},
   },
   reducers: {
     create_farmer(state,action){
@@ -14,47 +14,12 @@ export const FarmerDetails = createSlice({
     create_id(state,action){
       state.farmer_id = action.payload;
     },
+    edit_data(state,action){
+      state.editData = action.payload;
+    },
   }   
  });
 
  export const farmeractions =  FarmerDetails.actions;
+
  export default FarmerDetails;
-
-//  function authReducer(state = initialAuthState, action) {
-//    switch (action.type) {
-//     case CREATE_FARMER:
-//       return { 
-//         ...state,
-//         farmer: [...state.farmer, action.payload]
-//     }
-//     case STATE:
-//       return { 
-//         ...state,
-//         state: action.payload,
-//     }
-//     case DISTRICT:
-//       return { 
-//         ...state,
-//         district: action.payload,
-//     }
-//     case UNION:
-//       return { 
-//         ...state,
-//         union: action.payload,
-//     }
-//     case PANCHAYAT:
-//       return { 
-//         ...state,
-//         panchayat: action.payload,
-//     }
-//     case VILLAGE:
-//       return { 
-//         ...state,
-//         village: action.payload,
-//     }
-//      default:
-//        return state;
-//    }
-//  }
-
-//  export default authReducer;
